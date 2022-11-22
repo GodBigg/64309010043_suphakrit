58548c77a29459ddaa8ee95c6fb623166e1ed58f
GodBigg <64309010043@tnk.ac.th>
2022-11-22 08:55:25 +0700

    Update WorkShop6.js

--- Week6/WorkShop6.js
+++ Week6/WorkShop6.js
@@ -1,4 +1,4 @@
-let item = {
+/*let item = {
     "A4paper": 120,
     "Ruler": 25,
     "FlashDrive": 130
@@ -12,3 +12,13 @@ var Order = [{
 }];
 
 console.log(Order[0])
+*/
+
+var order = {
+    "customer":"Mr.Lee",
+    "itemOrder": ["A4 paper","Ruler"],
+    "Qty":[5,2],
+    "price":[120,25]
+}
+
+console.log(order)
