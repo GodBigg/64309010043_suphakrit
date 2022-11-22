a37f1ed98ee55b740e426cca1d537e93cef934ca
GodBigg <64309010043@tnk.ac.th>
2022-11-22 09:27:23 +0700

    Update WorkShop6.js

--- Week6/WorkShop6.js
+++ Week6/WorkShop6.js
@@ -1,24 +1,47 @@
-/*let item = {
-    "A4paper": 120,
-    "Ruler": 25,
-    "FlashDrive": 130
-}
+let item = [
+    {
+        "name": "A4paper",
+        "price": 120
+    },
+    {
+        "name": "Ruler",
+        "price": 25
+    },
+    {
+        "name": "Flash Drive",
+        "price": 130
+    }
+]
 
 var Order = [{
-    "customer" : "Mr.Lee",
-    "itemOrder" : [item.A4paper, item.Ruler],
-    "Qty" : [5,2],
-    "Price" : itemOrder[0]*Qty[0]
-}];
-
-console.log(Order[0])
-*/
-
-var order = {
-    "customer":"Mr.Lee",
-    "itemOrder": ["A4 paper","Ruler"],
-    "Qty":[5,2],
-    "price":[120,25]
+    "customer": "Mr.Lee",
+    "itemOrder": [item[0].name, item[1].name],
+    "Qty": [5, 2],
+    "price": [item[0].price, item[1].price]
+},
+{
+    "customer": "Mr.Yue",
+    "itemOrder": [item[2].name, item[0].name],
+    "Qty": [2, 3],
+    "price": [item[2].price, item[0].price]
+},
+{
+    "customer": "Mr.Bigg",
+    "itemOrder": [item[0].name, item[1].name, item[2].name],
+    "Qty": [2, 3, 4],
+    "price": [item[0].price, item[1].price, item[2].price]
 }
+];
 
-console.log(order)
+console.log(Order[0].customer,
+    Order[0].price[0] * Order[0].Qty[0] +
+    Order[0].price[1] * Order[0].Qty[1])
+
+console.log(Order[1].customer,
+    Order[1].price[0] * Order[1].Qty[0] +
+    Order[1].price[1] * Order[1].Qty[1])
+
+console.log(Order[2].customer,
+    Order[2].price[0] * Order[2].Qty[0] +
+    Order[2].price[1] * Order[2].Qty[1] +
+    Order[2].price[2] * Order[2].Qty[2])
